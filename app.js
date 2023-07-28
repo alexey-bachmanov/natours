e7ed83feff66dd46fcdb0c5a6db24d1b9cff9ac8
Alexey Bachmanov <alexbachmanov@gmail.com>
2023-07-28 17:11:06 -0400

    lecture 146

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('./utils/xssCleaner');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
 
@@ -9,9 +16,36 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 ///// GLOBAL MIDDLEWARES /////
-app.use(express.json());
+// set security HTTP headers
+app.use(helmet());
+// rate limit
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this address',
+});
+app.use('/api', limiter);
+// parse body data into req.body
+app.use(express.json({ limit: '10kb' }));
+// sanitize data against NoSQL query injection and XSS (cross-site scripting) attacks
+app.use(mongoSanitize());
+app.use(xssClean());
+// prevent http parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'difficulty',
+      'maxGroupSize',
+      'price',
+    ],
+  })
+);
+// serve up static files
 app.use(express.static('./public'));
-app.use(morgan('dev'));
+// development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 ///// ROUTES /////
