c34f768be1ff3c1ec8ab59e5c1e35dd714737e98
Alexey Bachmanov <alexbachmanov@gmail.com>
2023-08-04 13:17:35 -0400

    lecture 167

--- models/tourModel.js
+++ models/tourModel.js
@@ -109,6 +109,10 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+///// INDICES /////
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 ///// VIRTUAL PROPERTIES /////
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
