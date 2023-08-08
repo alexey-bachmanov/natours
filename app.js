67417e13df3e9cc16465f38468e253911a0e0152
Alexey Bachmanov <alexbachmanov@gmail.com>
2023-08-07 23:31:04 -0400

    lecture 180

--- app.js
+++ app.js
@@ -66,6 +66,16 @@ app.get('/', (req, res) => {
     user: 'John',
   });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+  });
+});
 // api routes:
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
